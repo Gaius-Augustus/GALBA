a162f6707ba2f3e23981ab63f1bc8c236450cd49
Amrei Knuth <s-amknut@login-a.maas>
2024-10-14 16:58:14 +0000

    Braker Aufruf in submit.sh

--- submit.sh
+++ submit.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 #SBATCH -J my_job                            # job name
-#SBATCH -N 1                                  # number of nodes
-#SBATCH -n 1                                   # number of MPI processes, here 1 per node
-#SBATCH --partition=snowball         # choose nodes from partition
+#SBATCH -N 4                                  # number of nodes
+#SBATCH -n 4                                   # number of MPI processes, here 1 per node
+#SBATCH --partition=batch                  # choose nodes from partition
 #SBATCH -o %j.out                            # stdout file name (%j: job ID)
 #SBATCH -e %j.err                             # stderr file name (%j: job ID)
-#SBATCH -t 01:00:00                        # max run time (hh:mm:ss), max 72h!
+#SBATCH -t 24:00:00                        # max run time (hh:mm:ss), max 72h!
 #SBATCH --mail-type=end
 #SBATCH --mail-user=s-amknut@uni-greifswald.de
 
@@ -16,6 +16,24 @@ echo " "
 echo "jobname: $SLURM_JOB_NAME"
 
 ## load modules
+module load singularity
 #module load gcc/5.2.0   # for instance
 
-./bin/main.py /home/s-amknut/GALBA/bin/testGenome.fa /home/s-amknut/GALBA/bin/testGenome /home/s-amknut/GALBA/bin/testReads.fa
+#./scripts/main.py -g /home/s-amknut/GALBA/scripts/testGenome.fa -r /home/s-amknut/GALBA/scripts/shorttestrna1.fq
+#/home/nas-hs/projs/galba-isoseq/data/Arabidopsis_thaliana/rnaseq/SRR12076896_1.fastq
+#/home/nas-hs/projs/galba-isoseq/data/Arabidopsis_thaliana/genome/genome.fasta.masked
+#./scripts/main.py -g /home/nas-hs/projs/galba-isoseq/data/Arabidopsis_thaliana/genome/genome.fasta.masked -r /home/nas-hs/projs/galba-isoseq/data/Arabidopsis_thaliana/rnaseq/SRR12076896_1.fastq
+#singularity exec -B $PWD:$PWD galba.sif /home/s-amknut/GALBA/scripts/main.py -g /home/nas-hs/projs/galba-isoseq/data/Arabidopsis_thaliana/genome/genome.fasta.masked -r /home/nas-hs/projs/galba-isoseq/data/Arabidopsis_thaliana/rnaseq/SRR12076896_1.fastq
+#singularity exec -B $PWD:$PWD galba.sif /home/s-amknut/GALBA/scripts/main.py -g /home/s-amknut/GALBA/entireGenome.fasta -s /home/s-amknut/GALBA/entireRna1.fastq -l /home/s-amknut/GALBA/entireLongRna.fsa
+#./scripts/main.py -y arabidopsis.yaml
+# ./braker.pl --species=Arabidopsis_thaliana --genome=/home/nas-hs/projs/galba-isoseq/data/Arabidopsis_thaliana/genome/genome.fasta.masked --rnaseq_sets_ids=SRR12076896_1 --rnaseq_sets_dirs=/home/nas-hs/projs/galba-isoseq/data/Arabidopsis_thaliana/rnaseq/
+WD=/home/nas-hs/projs/galba-isoseq/data/Arabidopsis_thaliana
+#singularity exec -B $WD galba.sif /home/s-amknut/GALBA/scripts/main.py -t 16 -y /home/s-amknut/GALBA/arabidopsis.yaml --mixed
+#singularity exec -B $WD galba.sif /home/s-amknut/GALBA/tools/BRAKER/scripts/braker.pl --species=Arabidopsis_thaliana_full --genome=/home/nas-hs/projs/galba-isoseq/data/Arabidopsis_thaliana/genome/genome.fasta.masked --rnaseq_sets_ids=SRR12076896_1 --rnaseq_sets_dirs=/home/nas-hs/projs/galba-isoseq/data/Arabidopsis_thaliana/rnaseq/ --GENEMARK_PATH=/home/s-amknut/GALBA/tools/GeneMark-ETP/bin/gmes/ --BAMTOOLS_PATH=/home/s-amknut/GALBA/tools/bamtools/build/src/
+
+#Command wurde ausgeführt und hat braker directory produziert:
+singularity exec -B $WD galba.sif /home/s-amknut/GALBA/tools/BRAKER/scripts/braker.pl --species=Arabidopsis_thaliana --useexisting --genome=/home/nas-hs/projs/galba-isoseq/data/Arabidopsis_thaliana/genome/genome.fasta.masked --rnaseq_sets_ids=SRR12076896_1,SRR12076896_2,SRR12547664_1,SRR12547664_2,SRR4010853_1,SRR4010853_2,SRR7289569_1,SRR7289569_2,SRR8714016_1,SRR8714016_2,SRR8759751_1,SRR8759751_2 --rnaseq_sets_dirs=/home/nas-hs/projs/galba-isoseq/data/Arabidopsis_thaliana/rnaseq/ --GENEMARK_PATH=/home/s-amknut/GALBA/tools/GeneMark-ETP/bin/gmes/ --BAMTOOLS_PATH=/home/s-amknut/GALBA/tools/bamtools/build/src/ --PROTHINT_PATH=/home/s-amknut/GALBA/tools/ProtHint/bin/ --prot_seq=/home/nas-hs/projs/galba-isoseq/data/Arabidopsis_thaliana/proteins/proteins.fa 
+
+
+
+
